6b76af427a307650fc1d24e5921c4b5e2fd751b1
Juan Manuel Musacchio <jmusacchio@foldersoluciones.com>
2015-11-25 15:22:40 -0300

    Updated vertx event bus handlers documentation.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jmusacchio:vertxbus',
-  version: '0.0.4',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'The Vert.x Event Bus Package allows any meteor app to be able to connect to Vert.x Components',
   // URL to the Git repository containing the source code for this package.
