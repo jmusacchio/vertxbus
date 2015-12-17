bad95c3e964141730c69b6944b582c8b85eeddd3
Juan Manuel Musacchio <jmusacchio@foldersoluciones.com>
2015-12-16 22:27:44 -0300

    Updated vertx3-eventbus-client to 3.2.0 version.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jmusacchio:vertxbus',
-  version: '0.0.5',
+  version: '0.0.6',
   // Brief, one-line summary of the package.
   summary: 'The Vert.x Event Bus Package allows any meteor app to be able to connect to Vert.x Components',
   // URL to the Git repository containing the source code for this package.
@@ -12,7 +12,7 @@ Package.describe({
 
 Npm.depends({
   'sockjs-client': '1.0.3',
-  'vertx3-eventbus-client': '3.1.0'
+  'vertx3-eventbus-client': '3.2.0'
 });
 
 Package.onUse(function(api) {
