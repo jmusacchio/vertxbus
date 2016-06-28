7c159f96ad2903c86a0c0831405c9570d4769804
Juan Manuel Musacchio <jmusacchio@foldersoluciones.com>
2016-06-28 10:46:57 -0300

    Upgraded all javascript versions to latest ones. Libraries, packages, etc. Should work with vertx 3.3.0 version.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jmusacchio:vertxbus',
-  version: '0.0.7',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'The Vert.x Event Bus Package allows any meteor app to be able to connect to Vert.x Components',
   // URL to the Git repository containing the source code for this package.
@@ -11,13 +11,13 @@ Package.describe({
 });
 
 Npm.depends({
-  'sockjs-client': '1.0.3',
-  'vertx3-eventbus-client': '3.2.1'
+  'sockjs-client': '1.1.1',
+  'vertx3-eventbus-client': '3.3.0'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
-  api.use(['cosmos:browserify@0.5.1'], 'client');
+  api.versionsFrom('1.2');
+  api.use(['cosmos:browserify@0.10.0'], 'client');
   api.addFiles('npm.browserify.js', 'client');
   api.addFiles('npm.js', 'server');
   api.addFiles('export.js');
